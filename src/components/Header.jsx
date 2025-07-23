64e506afea1c964aea01d90be5345b32e831a4b9
alvinisaac0602 <kiizaisaacalvin256@gmail.com>
2025-07-23 11:53:57 +0300

    seting up pages and contents

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto text-center">
+          <h1 className="text-2xl font-bold">Your Website Title</h1>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
